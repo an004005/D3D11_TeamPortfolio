98867d801ffda46fcae6880d07aed11c4dcd1e46
Jw-Bell9 <jw925@naver.com>
2023-03-14 20:45:56 +0900

    [feat] 병합

--- Client/private/SkummyPool.cpp
+++ Client/private/SkummyPool.cpp
@@ -81,16 +81,16 @@ HRESULT CSkummyPool::Initialize(void * pArg)
 			pBullet->Get_BulletEffect()->Start_NoAttach(pBullet, true);
 			// ~Effect
 
-		//	_matrix BoneMtx = m_pModelCom->GetBoneMatrix("Alga_F_03") * m_pTransformCom->Get_WorldMatrix();
-		//	_vector vPrePos = BoneMtx.r[3];
+			_matrix BoneMtx = m_pModelCom->GetBoneMatrix("Alga_F_03") * m_pTransformCom->Get_WorldMatrix();
+			_vector vPrePos = BoneMtx.r[3];
 
-		//	_vector vLook = m_pTransformCom->Get_State(CTransform::STATE_LOOK);
+			_vector vLook = m_pTransformCom->Get_State(CTransform::STATE_LOOK);
 
-		//	pBullet->GetTransform()->Set_State(CTransform::STATE_TRANSLATION, vPrePos);
-		//	pBullet->Set_ShootDir(vLook);
+			pBullet->GetTransform()->Set_State(CTransform::STATE_TRANSLATION, vPrePos);
+			pBullet->Set_ShootDir(vLook);
 
-		//	pBullet->GetTransform()->LookAt(vPrePos + vLook);
-		//}
+			pBullet->GetTransform()->LookAt(vPrePos + vLook);
+		}
 	});
 	m_pModelCom->Add_EventCaller("Upper", [this]
 	{
