ed20c7bbe546f1d2659cbbbf9794e41b73d8a4fb
안중환 <an004005@gmail.com>
2023-03-25 22:39:22 +0900

    [feat] 팁트리거 추가

--- Client/public/Level_Tutorial.h
+++ Client/public/Level_Tutorial.h
@@ -19,12 +19,12 @@ public:
 private:
 	HRESULT Ready_Prototypes();
 	HRESULT Ready_Lights();
-	HRESULT Ready_Layer_BackGround(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Camera(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Player(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_UI(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Map(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Effect(const _tchar* pLayerTag);
+	HRESULT Ready_Layer_SASPortrait();
 
 public:
 	static CLevel_Tutorial* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
