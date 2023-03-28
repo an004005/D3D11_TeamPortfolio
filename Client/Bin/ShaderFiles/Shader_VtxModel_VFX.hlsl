039114fa6c4a77821cdc4a1cf6489adabc853fd2
MATTTTJ <hoon3022@naver.com>
2023-03-28 14:55:54 +0900

    [Feat] Effect Work

--- Client/Bin/ShaderFiles/Shader_VtxModel_VFX.hlsl
+++ Client/Bin/ShaderFiles/Shader_VtxModel_VFX.hlsl
@@ -428,6 +428,26 @@ PS_OUT PS_DEFAULT_MODEL(PS_IN In)
 	return Out;
 }
 
+PS_OUT PS_SAS_DEAD_EF(PS_IN In)
+{
+	PS_OUT			Out = (PS_OUT)0;
+
+	float4 OriginTex = g_tex_0.Sample(LinearSampler, float2(In.vTexUV.x, In.vTexUV.y - g_float_0));
+	float4 Color = g_vec4_0;
+	float4 Blend = OriginTex * Color * 2.0f;
+	float4 Final = saturate(Blend);
+	float BlendAlpha = g_tex_1.Sample(LinearSampler, In.vTexUV).r;
+	Out.vColor = CalcHDRColor(Final, g_float_1);
+	Out.vColor.a = Out.vColor.a * BlendAlpha * g_float_2;
+
+	Out.vFlag = float4(0.f, 0.f, 0.f, 0.f);
+
+	if (Out.vColor.a <= 0.001f)
+		discard;
+
+	return Out;
+}
+
 PS_OUT PS_EM1100_ELEC_BULLET_EXPLODE(PS_IN In)
 {
 	PS_OUT			Out = (PS_OUT)0;
@@ -1065,5 +1085,19 @@ technique11 DefaultTechnique
 		HullShader = NULL;
 		DomainShader = NULL;
 		PixelShader = compile ps_5_0 PS_EM1100_ELEC_BULLET_EXPLODE();
+	}
+
+	//22
+	pass SasDead
+	{
+		SetRasterizerState(RS_NonCulling);
+		SetDepthStencilState(DS_Default, 0);
+		SetBlendState(BS_AlphaBlend, float4(0.0f, 0.f, 0.f, 0.f), 0xffffffff);
+
+		VertexShader = compile vs_5_0 VS_MAIN();
+		GeometryShader = NULL;
+		HullShader = NULL;
+		DomainShader = NULL;
+		PixelShader = compile ps_5_0 PS_SAS_DEAD_EF();
 	}
 }
