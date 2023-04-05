228ac30d4e03c5e574f23dc74e96e4494786ccc9
MATTTTJ <hoon3022@naver.com>
2023-04-05 22:16:41 +0900

    [Feat] Add TrailSystem_Ex

---
+++ Client/private/TrailSystem_EX.cpp
@@ -0,0 +1,252 @@
+#include "stdafx.h"
+#include "../public/TrailSystem_EX.h"
+#include "GameInstance.h"
+#include "VIBuffer_Trail_Instancing.h"
+#include "MathUtils.h"
+#include "GameUtils.h"
+#include "ImguiUtils.h"
+#include "JsonLib.h"
+
+CTrailSystem_EX::CTrailSystem_EX(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CGameObject(pDevice, pContext)
+{
+}
+
+CTrailSystem_EX::CTrailSystem_EX(const CTrailSystem_EX& rhs)
+	: CGameObject(rhs)
+{
+}
+
+HRESULT CTrailSystem_EX::Initialize(void* pArg)
+{
+	CGameObject::Initialize(pArg);
+
+	FAILED_CHECK(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_Renderer"), TEXT("Com_Renderer"), (CComponent**)&m_pRendererCom));
+	FAILED_CHECK(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_Shader_Trail_VFX"), TEXT("Shader"), (CComponent**)&m_pShaderCom));
+
+	m_pBuffer = CVIBuffer_Trail_Instancing::Create(m_pDevice, m_pContext, 300);
+
+	if (pArg)
+	{
+		Json& json = *static_cast<Json*>(pArg);
+
+		m_fLife = json["Life"];
+		m_fWidth = json["Width"];
+		m_fSegmentSize = json["SegmentSize"];
+		m_vColor = json["Color"];
+		if (json.contains("Pass"))
+			m_iPass = json["Pass"];
+
+		wstring texProtoTag = CGameUtils::s2ws(json["Texture"]);
+		FAILED_CHECK(__super::Add_Component(LEVEL_STATIC, texProtoTag.c_str(), TEXT("Texture"), (CComponent**)&m_pTex));
+		//FAILED_CHECK(__sLooker::Add_Component(LEVEL_STATIC, TEXT("../Bin/Resources/Texture/VFX/T_ef_one_lin_043.png"), TEXT("Texture"), (CComponent**)&m_pTex));
+	}
+	else
+	{
+		// FAILED_CHECK(__sLooker::Add_Component(LEVEL_STATIC, TEXT("../Bin/Resources/Texture/VFX/AlphaGradient/ring_glow.dds"), TEXT("Texture"),(CComponent**)&m_pTex));
+		FAILED_CHECK(__super::Add_Component(LEVEL_STATIC, TEXT("../Bin/Resources/Texture/VFX/T_ef_one_lin_043.png"), TEXT("Texture"), (CComponent**)&m_pTex));
+
+
+		m_fLife = 0.5f;
+		m_fWidth = 1.2f;
+		m_fSegmentSize = 0.1f;
+		m_vColor = _float4(1.f, 68.f / 255.f, 51.f / 255.f, 1.f);
+	}
+
+	m_pTransformCom->SetTransformDesc(CTransform::TRANSFORMDESC{ 15.f, XMConvertToRadians(180.f) });
+
+	return S_OK;
+}
+
+void CTrailSystem_EX::Tick(_double TimeDelta)
+{
+	CGameObject::Tick(TimeDelta);
+	m_pBuffer->Tick(TimeDelta);
+
+	if (m_bActive == false)
+		return;
+
+	_vector vPos = m_pTransformCom->Get_State(CTransform::STATE_TRANSLATION);
+	if (m_vPrePoses.empty() || m_vPrePoses.back() != vPos)
+		m_vPrePoses.push_back(vPos);
+
+	if (m_vPrePoses.size() >= 3)
+	{
+		while (m_vPrePoses.size() > 3)
+			m_vPrePoses.erase(m_vPrePoses.begin());
+
+		_vector vCamPos = CGameInstance::GetInstance()->Get_CamPosition();
+		_float fSplineLen = XMVectorGetX(XMVector3LengthEst(m_vPrePoses[1] - m_vPrePoses[2]));
+		_uint iSegmentCnt = static_cast<_uint>((fSplineLen / m_fSegmentSize));
+
+		_vector p0 = m_vPrePoses[0];
+		_vector p1 = m_vPrePoses[1];
+		_vector p2 = m_vPrePoses[2];
+		_vector p3 = m_vPrePoses[2];
+		_float fPreLife = 0.f;
+		if (m_pBuffer->GetDatas()->empty() == false)
+			fPreLife = m_pBuffer->GetDatas()->back().vPosition.w;
+
+		_vector vPrePos = p1;
+
+		for (_uint i = 0; i < iSegmentCnt; ++i)
+		{
+			_float fWeight = (_float)(i + 1) / (_float)iSegmentCnt;
+			_vector vSplinePos = XMVectorCatmullRom(p0, p1, p2, p3, fWeight);
+
+			_vector vRight = XMVector3Normalize(vSplinePos - vPrePos);
+			_vector vLookAtCam = XMVector3Normalize(vCamPos - vSplinePos);
+
+			float fRadian = XMConvertToDegrees(fabs(acosf(XMVectorGetX(XMVector3Dot(vLookAtCam, vRight)))));
+			if (fRadian < 5.f)
+				continue;
+
+			_vector vUp = XMVector3Cross(vRight, vLookAtCam);
+			_vector vLook = XMVector3Cross(vRight, vUp);
+			vSplinePos = XMVectorSetW(vSplinePos, CMathUtils::Lerp(fPreLife, m_fLife, fWeight));//life
+
+			_matrix TrailMatrix(vRight, vUp, vLook, vSplinePos);
+			m_pBuffer->AddData(TrailMatrix);
+
+			vPrePos = vSplinePos;
+		}
+	}
+}
+
+void CTrailSystem_EX::Late_Tick(_double TimeDelta)
+{
+	if (m_bVisible && m_pBuffer->IsEmpty() == false)
+		m_pRendererCom->Add_RenderGroup(CRenderer::RENDER_ALPHABLEND, this);
+}
+
+HRESULT CTrailSystem_EX::Render()
+{
+	if (FAILED(m_pTransformCom->Bind_ShaderResource(m_pShaderCom, "g_WorldMatrix")))
+		return E_FAIL;
+	if (FAILED(m_pShaderCom->Set_Matrix("g_ViewMatrix", &CGameInstance::GetInstance()->Get_TransformFloat4x4(CPipeLine::D3DTS_VIEW))))
+		return E_FAIL;
+	if (FAILED(m_pShaderCom->Set_Matrix("g_ProjMatrix", &CGameInstance::GetInstance()->Get_TransformFloat4x4(CPipeLine::D3DTS_PROJ))))
+		return E_FAIL;
+	if (FAILED(m_pShaderCom->Set_RawValue("g_vCamPosition", &CGameInstance::GetInstance()->Get_CamPosition(), sizeof(_float4))))
+		return E_FAIL;
+
+	auto pData = m_pBuffer->GetDatas();
+	m_pShaderCom->Set_MatrixArray("g_TrailData", (_float4x4*)pData->data(), static_cast<_uint>(pData->size()));
+	m_pShaderCom->Set_RawValue("g_fLife", &m_fLife, sizeof(_float));
+	m_pShaderCom->Set_RawValue("g_fWidth", &m_fWidth, sizeof(_float));
+	m_pShaderCom->Set_RawValue("g_Color", &m_vColor, sizeof(_float4));
+	m_pTex->Bind_ShaderResource(m_pShaderCom, "g_AlphaTex");
+
+	m_pShaderCom->Begin(m_iPass);
+	m_pBuffer->Render();
+
+	return S_OK;
+}
+
+void CTrailSystem_EX::SaveToJson(Json& json)
+{
+	json["Life"] = m_fLife;
+	json["Width"] = m_fWidth;
+	json["SegmentSize"] = m_fSegmentSize;
+	json["Color"] = m_vColor;
+	json["Pass"] = m_iPass;
+
+	char texProtoTag[MAX_PATH];
+	CGameUtils::wc2c(m_pTex->GetPrototypeTag(), texProtoTag);
+	json["Texture"] = texProtoTag;
+}
+
+void CTrailSystem_EX::Imgui_RenderProperty()
+{
+	ImGui::InputFloat("Life", &m_fLife);
+	ImGui::InputFloat("Width", &m_fWidth);
+	ImGui::InputFloat("Segment Size", &m_fSegmentSize);
+	ImGui::ColorEdit4("Color", (float*)&m_vColor);
+
+	_int iPass = m_iPass;
+	ImGui::InputInt("Pass", &iPass);
+	m_iPass = iPass < 0 ? 0 : iPass;
+
+	CImguiUtils::FileDialog_FileSelector("Select AlphaTexture", ".png,.dds", "../Bin/Resources/Textures/VFX/",
+		[this](const string& fileName)
+		{
+			size_t iPos = fileName.find("\\Bin\\Resources");
+			string relative = ".." + fileName.substr(iPos, fileName.size() - iPos);
+
+			Safe_Release(m_pTex);
+			FAILED_CHECK(Delete_Component(L"Texture"));
+			FAILED_CHECK(Add_Component(LEVEL_NOW, CGameUtils::s2ws(relative).c_str(), TEXT("Texture"),
+				(CComponent**)&m_pTex));
+
+		});
+
+	CImguiUtils::FileDialog_FileSelector("Save This Trail", ".json", "../Bin/Resources/VFX/Trail/",
+		[this](const string& filePath)
+		{
+			Json json;
+			SaveToJson(json);
+			std::ofstream file(filePath);
+			file << json;
+		});
+
+
+	ImGui::Separator();
+	CGameObject::Imgui_RenderProperty();
+}
+
+HRESULT CTrailSystem_EX::SetUp_ShaderResources()
+{
+	FAILED_CHECK(m_pTransformCom->Bind_ShaderResource(m_pShaderCom, "g_WorldMatrix"));
+	FAILED_CHECK(m_pShaderCom->Set_Matrix("g_ViewMatrix", &CGameInstance::GetInstance()->Get_TransformFloat4x4(CPipeLine::D3DTS_VIEW)));
+	FAILED_CHECK(m_pShaderCom->Set_Matrix("g_ProjMatrix", &CGameInstance::GetInstance()->Get_TransformFloat4x4(CPipeLine::D3DTS_PROJ)));
+	FAILED_CHECK(m_pShaderCom->Set_RawValue("g_vCamPosition", &CGameInstance::GetInstance()->Get_CamPosition(), sizeof(_float4)));
+
+	auto pData = m_pBuffer->GetDatas();
+
+	FAILED_CHECK(m_pShaderCom->Set_MatrixArray("g_TrailData", (_float4x4*)pData->data(), static_cast<_uint>(pData->size())));
+	FAILED_CHECK(m_pShaderCom->Set_RawValue("g_fLife", &m_fLife, sizeof(_float)));
+	FAILED_CHECK(m_pShaderCom->Set_RawValue("g_fWidth", &m_fWidth, sizeof(_float)));
+	FAILED_CHECK(m_pShaderCom->Set_RawValue("g_Color", &m_vColor, sizeof(_float4)));
+	FAILED_CHECK(m_pTex->Bind_ShaderResource(m_pShaderCom, "g_AlphaTex"));
+
+	return S_OK;
+}
+
+void CTrailSystem_EX::ClearBuffer()
+{
+	m_pBuffer->ClearBuffer();
+	m_vPrePoses.clear();
+}
+
+CTrailSystem_EX* CTrailSystem_EX::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	CTrailSystem_EX* pInstance = new CTrailSystem_EX(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX("Failed to Created : CTrailSystem_EX");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+CGameObject* CTrailSystem_EX::Clone(void* pArg)
+{
+	CTrailSystem_EX* pInstance = new CTrailSystem_EX(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX("Failed to Cloned : CTrailSystem_EX");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+void CTrailSystem_EX::Free()
+{
+	CGameObject::Free();
+	Safe_Release(m_pBuffer);
+	Safe_Release(m_pShaderCom);
+	Safe_Release(m_pRendererCom);
+	Safe_Release(m_pTex);
+}
--- Client/public/TrailSystem_EX.h
+++ Client/public/TrailSystem_EX.h
@@ -1,5 +1,60 @@
 #pragma once
-class TrailSystem_EX
+#include "Client_Defines.h"
+#include "GameObject.h"
+#include "Shader.h"
+
+BEGIN(Engine)
+class CShader;
+class CTexture;
+class CRenderer;
+class CVIBuffer;
+END
+BEGIN(Client)
+class CTrailSystem_EX : public CGameObject
 {
+public:
+	CTrailSystem_EX(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CTrailSystem_EX(const CTrailSystem_EX& rhs);
+	virtual ~CTrailSystem_EX() = default;
+
+public:
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void	Tick(_double TimeDelta) override;
+	virtual void	Late_Tick(_double TimeDelta) override;
+	virtual HRESULT Render() override;
+	virtual void	SaveToJson(Json & json) override;
+	virtual void	Imgui_RenderProperty() override;
+	HRESULT			SetUp_ShaderResources();
+
+public:
+	void			SetActive(_bool bActive) { m_bActive = bActive; }
+	_bool			IsActive() const { return m_bActive; }
+
+	void			ClearBuffer();
+
+private:
+	CShader*		m_pShaderCom = nullptr;
+	CRenderer*		m_pRendererCom = nullptr;
+	class CVIBuffer_Trail_Instancing* m_pBuffer = nullptr;
+	class CTexture* m_pTex = nullptr;
+
+	_bool								m_bActive = true;
+
+	_float								m_fLife;
+	_float								m_fWidth;
+	_float								m_fSegmentSize;
+	_float4								m_vColor;
+	vector<_float4>						m_vPrePoses;
+	vector<_float4>						m_vPreLook;
+	list<_vector>						m_TrailPointList;
+	_uint								m_iPass = 0;
+	_uint								m_iSegmentCnt = 0;
+
+public:
+	static CTrailSystem_EX* Create(ID3D11Device * pDevice, ID3D11DeviceContext * pContext);
+	virtual CGameObject* Clone(void* pArg) override;
+	virtual void						Free() override;
 };
 
+
+END
