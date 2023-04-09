859d2c26c9eed51b435c7fc3cc93ba720e6c7801
Jeon <chouper@naver.com>
2023-04-09 20:46:02 +0900

    임시저장

--- Client/private/EM1200_AnimInstance.cpp
+++ Client/private/EM1200_AnimInstance.cpp
@@ -21,31 +21,12 @@ HRESULT CEM1200_AnimInstance::Initialize(CModel * pModel, CGameObject * pGameObj
 				.Predicator([this] { return !m_bChangePhase && m_bMove; })
 				.Duration(0.1f)
 
-			.AddTransition("Idle to Turn_R1", "Turn_R1")
-				.Predicator([this] { return !m_bChangePhase && m_eTurn == EBaseTurn::TURN_RIGHT; })
+			.AddTransition("Idle to Turn_R", "Turn_R")
+				.Predicator([this] { return m_eTurn == EBaseTurn::TURN_RIGHT; })
 				.Duration(0.1f)
 
-			.AddTransition("Idle to Turn_L1", "Turn_L1")
-				.Predicator([this] { return !m_bChangePhase && m_eTurn == EBaseTurn::TURN_LEFT; })
-				.Duration(0.1f)
-
-			.AddTransition("Idle to Idle2", "Idle2")
-				.Predicator([this] { return m_bChangePhase; })
-				.Duration(0.1f)
-
-		.AddState("Idle2")
-			.SetAnimation(*m_pModel->Find_Animation("AS_em1200_170_AL_wait02"))
-
-			.AddTransition("Idle2 to Run", "Run")
-				.Predicator([this] { return m_bRun; })
-				.Duration(0.1f)
-
-			.AddTransition("Idle2 to Turn_R2", "Turn_R2")
-				.Predicator([this] { return m_bChangePhase && m_eTurn == EBaseTurn::TURN_RIGHT; })
-				.Duration(0.1f)
-
-			.AddTransition("Idle2 to Turn_L2", "Turn_L2")
-				.Predicator([this] { return m_bChangePhase && m_eTurn == EBaseTurn::TURN_LEFT; })
+			.AddTransition("Idle to Turn_L", "Turn_L")
+				.Predicator([this] { return m_eTurn == EBaseTurn::TURN_LEFT; })
 				.Duration(0.1f)
 
 		.AddState("Walk")
@@ -54,35 +35,17 @@ HRESULT CEM1200_AnimInstance::Initialize(CModel * pModel, CGameObject * pGameObj
 				.Predicator([this] { return !m_bMove; })
 				.Duration(0.1f)
 
-		.AddState("Run")
-			.SetAnimation(*m_pModel->Find_Animation("AS_em1200_184_AL_run02_loop"))
-			.AddTransition("Run to Idle2", "Idle2")
-				.Predicator([this] { return !m_bRun; })
-				.Duration(0.1f)
-
-		.AddState("Turn_R1")
+		.AddState("Turn_R")
 			.SetAnimation(*m_pModel->Find_Animation("AS_em1200_153_AL_turn_R_loop1"))
 			.AddTransition("Turn_R1 to Idle", "Idle")
-				.Predicator([this] { return m_bChangePhase || m_eTurn != EBaseTurn::TURN_RIGHT; })
-				.Duration(0.2f)
+				.Predicator([this] { return m_eTurn != EBaseTurn::TURN_RIGHT; })
+				.Duration(0.f)
 
-		.AddState("Turn_L1")
+		.AddState("Turn_L")
 			.SetAnimation(*m_pModel->Find_Animation("AS_em1200_151_AL_turn_L_loop1"))
 			.AddTransition("Turn_L1 to Idle", "Idle")
-				.Predicator([this] { return m_bChangePhase || m_eTurn != EBaseTurn::TURN_LEFT; })
-				.Duration(0.2f)
-
-		.AddState("Turn_R2")
-			.SetAnimation(*m_pModel->Find_Animation("AS_em1200_157_AL_turn_R_loop2"))
-			.AddTransition("Turn_R2 to Idle2", "Idle2")
-				.Predicator([this] { return m_eTurn != EBaseTurn::TURN_RIGHT; })
-				.Duration(0.1f)
-
-		.AddState("Turn_L2")
-			.SetAnimation(*m_pModel->Find_Animation("AS_em1200_155_AL_turn_L_loop2"))
-			.AddTransition("Turn_L2 to Idle2", "Idle2")
 				.Predicator([this] { return m_eTurn != EBaseTurn::TURN_LEFT; })
-				.Duration(0.1f)
+				.Duration(0.f)
 
 		.Build();
 
@@ -98,7 +61,56 @@ void CEM1200_AnimInstance::UpdateTargetState(_double TimeDelta)
 	m_bMove = pEM1200->IsMove();
 	m_bRun = pEM1200->IsRun();
 	m_eTurn = pEM1200->GetBaseTurn();
-	m_bChangePhase = pEM1200->IsChangePhase();
+	
+	if (m_bChangePhase == false && pEM1200->IsChangePhase() == true)
+	{
+		Make2PhaseASM();
+		m_bChangePhase = true;
+	}
+}
+
+void CEM1200_AnimInstance::Make2PhaseASM()
+{
+	Safe_Release(m_pASM_Base);
+
+	m_pASM_Base = CASMBuilder()
+		.InitState("Idle")
+		.AddState("Idle")
+		.SetAnimation(*m_pModel->Find_Animation("AS_em1200_170_AL_wait02"))
+
+		.AddTransition("Idle to Run", "Run")
+			.Predicator([this] { return m_bRun; })
+			.Duration(0.1f)
+
+		.AddTransition("Idle to Turn_R", "Turn_R")
+			.Predicator([this] { return m_eTurn == EBaseTurn::TURN_RIGHT; })
+			.Duration(0.1f)
+
+		.AddTransition("Idle to Turn_L", "Turn_L")
+			.Predicator([this] { return m_eTurn == EBaseTurn::TURN_LEFT; })
+			.Duration(0.1f)
+
+		.AddState("Run")
+		.SetAnimation(*m_pModel->Find_Animation("AS_em1200_184_AL_run02_loop"))
+			.AddTransition("Run to Idle", "Idle")
+			.Predicator([this] { return !m_bRun; })
+			.Duration(0.1f)
+
+		.AddState("Turn_R")
+		.SetAnimation(*m_pModel->Find_Animation("AS_em1200_157_AL_turn_R_loop2"))
+			.AddTransition("Turn_R1 to Idle", "Idle")
+			.Predicator([this] { return m_eTurn != EBaseTurn::TURN_RIGHT; })
+			.Duration(0.f)
+
+		.AddState("Turn_L")
+		.SetAnimation(*m_pModel->Find_Animation("AS_em1200_155_AL_turn_L_loop2"))
+			.AddTransition("Turn_L1 to Idle", "Idle")
+			.Predicator([this] { return m_eTurn != EBaseTurn::TURN_LEFT; })
+			.Duration(0.f)
+
+		.Build();
+
+	m_mapAnimSocket.insert({ "FullBody",{} });
 }
 
 CEM1200_AnimInstance * CEM1200_AnimInstance::Create(CModel * pModel, CGameObject * pGameObject)
--- Engine/public/HelperClasses.h
+++ Engine/public/HelperClasses.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <functional>
 
 BEGIN(Engine)
 class CDoOnce
@@ -14,9 +15,20 @@ public:
 			return true;
 		}
 	}
+
+	void IsNotDo(std::function<void()> func)
+	{
+		if (!m_bDoFunc) {
+			func();
+			m_bDoFunc = true;
+		}
+	}
+
 	void Reset() { m_bDo = false; }
+	void ResetFunc() { m_bDoFunc = false; }
 
 private:
 	_bool m_bDo = false;
+	_bool m_bDoFunc = false;
 };
 END
