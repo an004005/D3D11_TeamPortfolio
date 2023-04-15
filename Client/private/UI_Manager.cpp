22ba969064a13405ccb738294c98e7c4a8f54253
SuHyeonOk <tntngus1223@naver.com>
2023-04-15 19:51:53 +0900

    [Feat] Logo

--- Client/private/UI_Manager.cpp
+++ Client/private/UI_Manager.cpp
@@ -9,29 +9,29 @@ IMPLEMENT_SINGLETON(CUI_Manager)
 
 CUI_Manager::CUI_Manager()
 {
-	m_SoundStore.CloneSound("UIClick");		// 메인에서 클릭할 때
-	m_SoundStore.CloneSound("UIClick2");	// 불가능한 클릭
-	m_SoundStore.CloneSound("UIClick2");	// 파티 클릭 할 때
-	m_SoundStore.CloneSound("UIClick2");	// 상태이상 소리 긴 것
-	m_SoundStore.CloneSound("UIClick2");	// 상태이상 소리 짧은 것
-	m_SoundStore.CloneSound("UIClick2");	// 아이템 넘길 때
-	m_SoundStore.CloneSound("UIClick2");	// 아이템 사용 불가
-	m_SoundStore.CloneSound("UIClick2");	// 아이템 사용
-	m_SoundStore.CloneSound("UIClick2");	// 메인 들어갈 때
-	m_SoundStore.CloneSound("UIClick2");	// 메인 나갈 때
-	m_SoundStore.CloneSound("UIClick2");	// 몬스터 경고 (몬스터 발견 했을 때?)
-	m_SoundStore.CloneSound("UIClick2");	// 안내창 뜰 때
-	m_SoundStore.CloneSound("UIClick2");	// 안내칭 끌 때
-	m_SoundStore.CloneSound("UIClick2");	// art
-	m_SoundStore.CloneSound("UIClick2");	// ctrl
-	m_SoundStore.CloneSound("UIClick2");	// x
-	m_SoundStore.CloneSound("UIClick2");	// 마우스 클릭?
-	m_SoundStore.CloneSound("UIClick2");	// 튜토리얼 켤 때
-	m_SoundStore.CloneSound("UIClick2");	// 튜토리얼 끌 때
-	m_SoundStore.CloneSound("UIClick2");	// Tip 뜰 때
-	m_SoundStore.CloneSound("UIClick2");	// 불가능한 클릭
-	m_SoundStore.CloneSound("UIClick2");	// 레벨 업 할 때
-	m_SoundStore.CloneSound("UIClick2");	// 장비 장착할 때
+	//m_SoundStore.CloneSound("UIClick");		// 메인에서 클릭할 때
+	//m_SoundStore.CloneSound("UIClick2");	// 불가능한 클릭
+	//m_SoundStore.CloneSound("UIClick2");	// 파티 클릭 할 때
+	//m_SoundStore.CloneSound("UIClick2");	// 상태이상 소리 긴 것
+	//m_SoundStore.CloneSound("UIClick2");	// 상태이상 소리 짧은 것
+	//m_SoundStore.CloneSound("UIClick2");	// 아이템 넘길 때
+	//m_SoundStore.CloneSound("UIClick2");	// 아이템 사용 불가
+	//m_SoundStore.CloneSound("UIClick2");	// 아이템 사용
+	//m_SoundStore.CloneSound("UIClick2");	// 메인 들어갈 때
+	//m_SoundStore.CloneSound("UIClick2");	// 메인 나갈 때
+	//m_SoundStore.CloneSound("UIClick2");	// 몬스터 경고 (몬스터 발견 했을 때?)
+	//m_SoundStore.CloneSound("UIClick2");	// 안내창 뜰 때
+	//m_SoundStore.CloneSound("UIClick2");	// 안내칭 끌 때
+	//m_SoundStore.CloneSound("UIClick2");	// art
+	//m_SoundStore.CloneSound("UIClick2");	// ctrl
+	//m_SoundStore.CloneSound("UIClick2");	// x
+	//m_SoundStore.CloneSound("UIClick2");	// 마우스 클릭?
+	//m_SoundStore.CloneSound("UIClick2");	// 튜토리얼 켤 때
+	//m_SoundStore.CloneSound("UIClick2");	// 튜토리얼 끌 때
+	//m_SoundStore.CloneSound("UIClick2");	// Tip 뜰 때
+	//m_SoundStore.CloneSound("UIClick2");	// 불가능한 클릭
+	//m_SoundStore.CloneSound("UIClick2");	// 레벨 업 할 때
+	//m_SoundStore.CloneSound("UIClick2");	// 장비 장착할 때
 
 }
 
